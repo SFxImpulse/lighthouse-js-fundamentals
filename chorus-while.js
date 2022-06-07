996a687e223d3beb9f87a4517dbd53e24ae83ecb
SFxImpulse <braverman.david.business@gmail.com>
2022-06-07 20:55:02 +0000

    Added a for loop to the chrous-loop

--- chorus-while.js
+++ chorus-while.js
@@ -1,3 +1,7 @@
+// Chorus-loop with 'while'.
+
+/*
+
 const chorus = "Let's dance!";
 let repeat = 0;
 while (repeat < 10) {
@@ -7,4 +11,15 @@ while (repeat < 10) {
   console.log(chorus);
   repeat++;
 }
+console.log("Until the sun comes up!");
+
+*/
+
+// Chorus-loop with 'for'
+
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+
 console.log("Until the sun comes up!");
