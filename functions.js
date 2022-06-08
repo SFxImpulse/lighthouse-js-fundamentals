684f3653b73965fd3512b02d2c93b1313f3e2f14
SFxImpulse <braverman.david.business@gmail.com>
2022-06-08 14:49:00 +0000

    Added a function that tells you if a number is even or not

--- functions.js
+++ functions.js
@@ -1,6 +1,50 @@
+// Basic logging function.
+
+/*
+
 const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 
 sayHello('David');
 sayHello('Rae');
+
+*/
+
+// Basic logging function vs a return function.
+
+/*
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole('David');
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('Rae');
+
+console.log(greeting);
+
+*/
+
+/* 
+
+isEven function: returns true or false depending 
+on the number being even or not.
+
+*/
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
